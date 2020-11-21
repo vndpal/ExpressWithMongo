e56aa14705f1e99215cdbfca634656259151dc72
VNDPAL <39481600+VNDPAL@users.noreply.github.com>
2020-11-21 18:19:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const url = "mongodb+srv://mongo:mongo@cluster0.lmmmb.mongodb.net/myDB?retryWrites=true&w=majority";
 
+var port = process.env.PORT || 8080;
 const app = express()
 
 mongoose.connect(url, { useNewUrlParser: true })
@@ -16,6 +17,6 @@ app.use(express.json())
 const alienRouter = require('./routers/aliens')
 app.use('/aliens', alienRouter)
 
-app.listen(9000, () => {
+app.listen(port, () => {
     console.log('sever started...')
 })
