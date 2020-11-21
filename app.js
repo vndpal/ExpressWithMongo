29b9248e08fb6da5f2eb5c0994c76c9dc90b8502
vndpal <undervinod@gmail.com>
2020-11-21 20:25:56 +0530

    sdfsfdsf

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
 const express = require('express')
-// const mongoose = require('mongoose')
-// const url = "mongodb+srv://mongo:mongo@cluster0.lmmmb.mongodb.net/myDB?retryWrites=true&w=majority";
+const mongoose = require('mongoose')
+const url = "mongodb+srv://mongo:mongo@cluster0.lmmmb.mongodb.net/myDB?retryWrites=true&w=majority";
 
 var port = process.env.PORT || 8080;
 const app = express()
 
 
-// mongoose.connect(url, { useNewUrlParser: true })
-// const con = mongoose.connection
+mongoose.connect(url, { useNewUrlParser: true })
+const con = mongoose.connection
 
-// con.on('open', () => {
-//     console.log('connected')
-// })
+con.on('open', () => {
+    console.log('connected')
+})
 
-// app.use(express.json())
+app.use(express.json())
 
-// const alienRouter = require('./routers/aliens')
-// app.use('/aliens', alienRouter)
-app.get('/', (req, res) => res.send('Working!!!'));
+const alienRouter = require('./routers/aliens')
+app.use('/aliens', alienRouter)
 
 app.listen(port, () => {
     console.log('sever started...')
--- routers/aliens.js
+++ routers/aliens.js
@@ -1,40 +1,32 @@
 const express = require('express')
 const router = express.Router()
-// const Alien = require('../models/alien')
+const Alien = require('../models/alien')
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     try {
-        res.send('ok cool')
+        const aliens = await Alien.find()
+        res.json(aliens)
     }
     catch (err) {
         res.send('Error: ' + err)
     }
 })
-// router.get('/', async (req, res) => {
-//     try {
-//         const aliens = await Alien.find()
-//         res.json(aliens)
-//     }
-//     catch (err) {
-//         res.send('Error: ' + err)
-//     }
-// })
 
-// router.post('/', async (req, res) => {
-//     const alien = new Alien({
-//         name: req.body.name,
-//         tech: req.body.tech,
-//         sub: req.body.sub
-//     })
+router.post('/', async (req, res) => {
+    const alien = new Alien({
+        name: req.body.name,
+        tech: req.body.tech,
+        sub: req.body.sub
+    })
 
-//     try {
-//         const a1 = await alien.save()
-//         res.json(a1)
-//     }
-//     catch (err) {
-//         res.send('Error: ' + err)
-//     }
-// })
+    try {
+        const a1 = await alien.save()
+        res.json(a1)
+    }
+    catch (err) {
+        res.send('Error: ' + err)
+    }
+})
 
 
 module.exports = router
