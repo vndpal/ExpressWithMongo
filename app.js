dccbf3cd50591cb9f62c0ea48a1b1d139650c389
vndpal <undervinod@gmail.com>
2020-11-21 18:42:04 +0530

    test4

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const mongoose = require('mongoose')
-const url = "mongodb+srv://mongo:mongo@cluster0.lmmmb.mongodb.net/myDB?retryWrites=true&w=majority";
+// const mongoose = require('mongoose')
+// const url = "mongodb+srv://mongo:mongo@cluster0.lmmmb.mongodb.net/myDB?retryWrites=true&w=majority";
 
 var port = process.env.PORT || 8080;
 const app = express()
@@ -12,10 +12,11 @@ const app = express()
 //     console.log('connected')
 // })
 
-app.use(express.json())
+// app.use(express.json())
 
-const alienRouter = require('./routers/aliens')
-app.use('/aliens', alienRouter)
+// const alienRouter = require('./routers/aliens')
+// app.use('/aliens', alienRouter)
+app.get('/', (req, res) => res.send('Working!!!'));
 
 app.listen(port, () => {
     console.log('sever started...')
