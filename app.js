ace0ac44ead4331b5bbf70475afaed52be77e20e
vndpal <undervinod@gmail.com>
2020-11-21 17:02:58 +0530

    express with mongo mongoose

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const url = "mongodb+srv://mongo:mongo@cluster0.lmmmb.mongodb.net/myDB?retryWrites=true&w=majority";
+
+const app = express()
+
+mongoose.connect(url, { useNewUrlParser: true })
+const con = mongoose.connection
+
+con.on('open', () => {
+    console.log('connected')
+})
+
+app.use(express.json())
+
+const alienRouter = require('./routers/aliens')
+app.use('/aliens', alienRouter)
+
+app.listen(9000, () => {
+    console.log('sever started...')
+})
